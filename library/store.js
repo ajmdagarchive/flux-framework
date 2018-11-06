8e8cb3f745419e2cc33520ded7f332c5ab0c1128
Yury Lavrukhin <yurka7321@gmail.com>
2018-11-06 03:33:00 +0300

    Add library

---
+++ library/store.js
@@ -0,0 +1,23 @@
+export default class Store {
+  constructor(updateState, state) {
+    this._updateState = updateState;
+    this._state = state;
+    this._callbacks = [];
+  }
+
+  get getState() {
+    return this._state;
+  }
+
+  update(action) {
+    this._state = this._updateState(this._state, action);
+    this._callbacks.forEach(cb => {
+      cb();
+    });
+  }
+
+  subscribe(callback) {
+    this._callbacks.push(callback);
+    return () => this._callbacks = this._callbacks.filter(cb => cb !== callback);
+  }
+}
